db89a032a099be056e6107efbd9b50e1615d1f66
Artem <artem.koh2020@gmail.com>
2026-03-30 14:38:13 +0300

    fixed zone.js issue and verified UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,10 @@
-export interface AppInfo {
-  title: string;
-  year: number;
-}
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes)
+  ]
+};
