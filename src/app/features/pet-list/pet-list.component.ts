f8fc1c56160101666cdaac12ddcd468d81d90143
Artem <artem.koh2020@gmail.com>
2026-04-01 18:41:47 +0300

    feat: add local search, status filtering and reset button

--- src/app/features/pet-list/pet-list.component.ts
+++ src/app/features/pet-list/pet-list.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PetCardComponent } from '../../shared/components/pet-card/pet-card.component';
 import { PETS } from '../../shared/mock-data';
+import { PetStatus } from '../../shared/models/pet.model';
 
 @Component({
   selector: 'pet-list',
   standalone: true,
-  imports: [CommonModule, PetCardComponent],
+  imports: [PetCardComponent, FormsModule],
   templateUrl: './pet-list.component.html',
   styleUrl: './pet-list.component.css'
 })
 export class PetListComponent {
-  pets = PETS;
-  handleCardAction(id: number) {
-    console.log(`Користувач натиснув кнопку на товарі з ID: ${id}`);
+  allPets = PETS;
+  filteredPets = [...this.allPets];
+
+  searchQuery = '';
+  selectedStatus = 'Всі';
+
+  statuses = ['Всі', PetStatus.Available, PetStatus.Adopted, PetStatus.Reserved];
+
+  filterItems() {
+    this.filteredPets = this.allPets.filter(pet => {
+      const matchesSearch = pet.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const matchesStatus = this.selectedStatus === 'Всі' || pet.status === this.selectedStatus;
+      
+      return matchesSearch && matchesStatus;
+    });
+  }
+
+  resetFilters(element: HTMLInputElement) {
+    this.searchQuery = '';
+    this.selectedStatus = 'Всі';
+    this.filterItems(); 
+    element.focus(); 
   }
 }
